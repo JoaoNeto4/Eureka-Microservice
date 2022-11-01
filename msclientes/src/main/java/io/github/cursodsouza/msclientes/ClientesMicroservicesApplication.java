f6f2922406607b7ef5c61f3fcc31ae54561f4de2
JoaoNeto4 <joaoquatlo@hotmail.com>
2022-11-01 14:15:04 -0300

    first commit

---
+++ msclientes/src/main/java/io/github/cursodsouza/msclientes/ClientesMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package io.github.cursodsouza.msclientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientesMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientesMicroservicesApplication.class, args);
+	}
+
+}
---
+++ msclientes/src/test/java/io/github/cursodsouza/msclientes/ClientesMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.cursodsouza.msclientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientesMicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
